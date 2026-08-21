a04f897c12f870f7714728510032f265390ddea2
Alexis Diaz <alexdan1993@gmail.com>
2026-08-21 13:42:35 -0300

    heart wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import "./App.css";
-import { SelectDropdownDemo } from "./components/select-dropdown/demo";
+// import { SelectDropdownDemo } from "./components/select-dropdown/demo";
 // import { CopyToClipboardDemo } from "./components/copy-to-clipboard/demo";
-// import { MagneticCursorDemo } from "./magnetic-cursor/demo1";
+import { HeartButton } from "./components/heart-btn-animated";
+
+function HeartBtnAnimatedDemo() {
+  return (
+    <>
+      <HeartButton />
+    </>
+  );
+}
 
 function App() {
-  return (
-    // <MagneticCursorDemo />
-    // <CopyToClipboardDemo />
-    <SelectDropdownDemo />
-  );
+  return <HeartBtnAnimatedDemo />;
 }
 
 export default App;
